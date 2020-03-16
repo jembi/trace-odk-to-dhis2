103735c1ced62d8d5d45cc334365073541285b4d
brad <bradford.sawadye@jembi.org>
2020-03-16 10:52:25 +0200

    add script for settig up everything

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+pathToFolder=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+# start up postgres
+echo 'Creating postgres container'
+sudo docker-compose -f "$pathToFolder/docker-compose-postgres.yml" up -d
+
+docker exec postgres bash -c "sleep 30s; su postgres -c 'psql postgres -c \"\\i create_db_and_user.sql;\"'"
+
+# start up ODK
+echo 'Creating the ODK container'
+sudo docker-compose -f "$pathToFolder/docker-compose-odk.yml" up -d
