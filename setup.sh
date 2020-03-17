41956dc76c662788681c5a0e9334d349591e228a
brad <bradford.sawadye@jembi.org>
2020-03-17 15:22:51 +0200

    add script for creating openhim and mapper
    The script also handles the configuration of the openhim

--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@ set -e
 
 pathToFolder=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
+# start up openhim
+sudo docker-compose -f "$pathToFolder/docker-compose.yml" up -d
+
 # start up postgres
 echo 'Creating postgres container'
 sudo docker-compose -f "$pathToFolder/docker-compose-postgres.yml" up -d
