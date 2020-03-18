68563e4ce7377615699dfba2dae26616bd4264f8
brad <bradford.sawadye@jembi.org>
2020-03-18 13:27:45 +0200

    fix the indentation and add new line

--- ODKAggregate/create_db_and_user.sql
+++ ODKAggregate/create_db_and_user.sql
@@ -1,9 +1,8 @@
 create database "odk";
-        create user "odk" with password 'odk';
-        grant all privileges on database "odk" to "odk";
-        alter database "odk" owner to "odk";
-        \c "odk";
-        create schema "odk";
-        grant all privileges on schema "odk" to "odk";
-        alter schema "odk" owner to "odk";
-      
+create user "odk" with password 'odk';
+grant all privileges on database "odk" to "odk";
+alter database "odk" owner to "odk";
+\c "odk";
+create schema "odk";
+grant all privileges on schema "odk" to "odk";
+alter schema "odk" owner to "odk";
